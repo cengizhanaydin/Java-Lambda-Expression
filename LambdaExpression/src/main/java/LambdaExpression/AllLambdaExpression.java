8cf56ff120042f0b3a7880f6470b6a00668008c5
cengizhan <cengizhan@192.168.1.123>
2018-05-20 23:55:01 +0300

    No Interface with Lambda Expression

--- LambdaExpression/src/main/java/LambdaExpression/AllLambdaExpression.java
+++ LambdaExpression/src/main/java/LambdaExpression/AllLambdaExpression.java
@@ -1,9 +1,16 @@
 package LambdaExpression;
 
+import java.util.function.Function;
+
 public class AllLambdaExpression {
 	
 	public void executeAnonim(Anonim anonim) {
 		anonim.call();
+	}
+	
+	public String executewithoutInterface(String string,Function <String,String> fn) {
+		
+		return fn.apply(string);
 	}
 
 	public static void main(String[] args) {
@@ -21,6 +28,11 @@ public class AllLambdaExpression {
 			}
 		};
 		
+		// Burda sadece fonksiyon kullanıyoruz.Interface kullanımı yok yukardaki örnekler gibi.
+		Function<String,String> fn = parameter -> parameter + " Hello!!";
+		
+		String result = expression.executewithoutInterface("Adam", fn);
+		System.out.println(result);
 		expression.executeAnonim(executeLambdaExpresion);
 		expression.executeAnonim(executeInterface);
 		
